d5e343158858cc7fe76c481252a54395e7aa4f14
dev5576 <pronay@enzan-k.com>
2021-05-13 10:54:46 +0900

    bloc pattern code added

---
+++ lib/counter_bloc.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+import 'package:test_app/counter_event.dart';
+
+class CounterBloc {
+  int _counter = 0;
+
+  final _counterStateController = StreamController<int>();
+
+  StreamSink<int> get _incCounter => _counterStateController.sink;
+
+  Stream<int> get counter => _counterStateController.stream;
+
+  final _counterEventController = StreamController<CounterEvent>();
+
+  StreamSink<CounterEvent> get counterEventSink => _counterEventController.sink;
+
+  CounterBloc() {
+    _counterEventController.stream.listen(_mapEventToState);
+  }
+
+  void _mapEventToState(CounterEvent event) {
+    if (event is IncrementEvent) {
+      _counter++;
+    }
+    if (event is DecrementEvent) {
+      _counter--;
+    }
+
+    _incCounter.add(_counter);
+  }
+
+  void dispose() {
+    _counterStateController.close();
+    _counterEventController.close();
+  }
+}
